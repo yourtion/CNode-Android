179ffc1e5887c552c1ed8f2f022cd223ad93f77d
YourtionGuo <yourtion@gmail.com>
2016-09-01 11:44:46 +0800

    update menu

--- app/src/main/java/com/yourtion/cnode_android/MainActivity.java
+++ app/src/main/java/com/yourtion/cnode_android/MainActivity.java
@@ -68,17 +68,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_all) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_essence) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_faq) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_jobs) {
 
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 

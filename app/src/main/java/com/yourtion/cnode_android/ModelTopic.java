fd9cb68fe184c7b78732c0c0a3d9f545ba546cb3
YourtionGuo <yourtion@gmail.com>
2016-09-01 15:36:39 +0800

    update module

--- app/src/main/java/com/yourtion/cnode_android/ModelTopic.java
+++ app/src/main/java/com/yourtion/cnode_android/ModelTopic.java
@@ -1,13 +1,40 @@
 package com.yourtion.cnode_android;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Yourtion on 9/1/16.
  */
 
 public class ModelTopic {
+
+    private static final String JSON_ID = "id";
+    private static final String JSON_AUTHOR_ID = "author_id";
+    private static final String JSON_TAB = "tab";
+    private static final String JSON_CONTENT = "content";
+    private static final String JSON_TITLE = "title";
+    private static final String JSON_LAST_REPLY_AT = "last_reply_at";
+    private static final String JSON_GOOD = "good";
+    private static final String JSON_TOP = "top";
+    private static final String JSON_REPLY_COUNT = "reply_count";
+    private static final String JSON_VISIT_COUNT = "visit_count";
+    private static final String JSON_CREATE_AT = "create_at";
+    private static final String JSON_AUTHOR = "author";
+    private static final String JSON_REPLIES = "replies";
+
+    private static final String JSON_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+
     public String mTopicId;
     public String mAuthorId;
     public String mTab;
@@ -21,6 +48,42 @@ public class ModelTopic {
     public Date mCreateAt;
     public ModelAuthor mAuthor;
     public ArrayList<ModelReplie> mModelReplies;
+
+    public ModelTopic(JSONObject json) throws JSONException {
+
+        DateFormat df = new SimpleDateFormat(JSON_DATE_FORMAT, Locale.CHINA);
+
+        mTopicId = json.getString(JSON_ID);
+        mAuthorId = json.getString(JSON_AUTHOR_ID);
+        mTab = json.optString(JSON_TAB, "unknow");
+        mContent = json.getString(JSON_CONTENT);
+        mTitle = json.getString(JSON_TITLE);
+        mGood = json.getBoolean(JSON_GOOD);
+        mTop = json.getBoolean(JSON_TOP);
+        mReplyCount = json.getInt(JSON_REPLY_COUNT);
+        mVisitCount = json.getInt(JSON_VISIT_COUNT);
+        try {
+            mLastReplyAt = df.parse(json.getString(JSON_LAST_REPLY_AT));
+            mCreateAt = df.parse(json.getString(JSON_CREATE_AT));
+        } catch (ParseException ignored) {
+            Log.e("EREEE", ignored.toString());
+        }
+
+
+        if (json.has(JSON_AUTHOR)) {
+            mAuthor = new ModelAuthor(json.getJSONObject(JSON_AUTHOR));
+        }
+
+        if (json.has(JSON_REPLIES)) {
+            mModelReplies = new ArrayList<>();
+            JSONArray jArray = json.getJSONArray(JSON_REPLIES);
+            if (jArray != null) {
+                for (int i = 0; i < jArray.length(); i++) {
+                    mModelReplies.add(new ModelReplie(jArray.getJSONObject(i)));
+                }
+            }
+        }
+    }
 
     public String getTopicId() {
         return mTopicId;
@@ -128,16 +191,16 @@ public class ModelTopic {
 }
 
 class ModelAuthor {
-    public String mAuthotId;
+
+    private static final String JSON_LOGINNAME = "loginname";
+    private static final String JSON_AVATAR_URL = "avatar_url";
+
     public String mLoginname;
     public String mAvatarUrl;
 
-    public String getAuthotId() {
-        return mAuthotId;
-    }
-
-    public void setAuthotId(String authotId) {
-        mAuthotId = authotId;
+    public ModelAuthor(JSONObject json) throws JSONException {
+        mLoginname = json.getString(JSON_LOGINNAME);
+        mAvatarUrl = json.getString(JSON_AVATAR_URL);
     }
 
     public String getLoginname() {
@@ -158,11 +221,44 @@ class ModelAuthor {
 }
 
 class ModelReplie {
+
+    private static final String JSON_ID = "id";
+    private static final String JSON_AUTHOR = "author";
+    private static final String JSON_CONTENT = "content";
+    private static final String JSON_UP = "ups";
+    private static final String JSON_CREATE_AT = "create_at";
+    private static final String JSON_REPLY_ID = "reply_id";
+
+    private static final String JSON_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+
     public String mReplieId;
     public ModelAuthor mAuthor;
+    public String mContent;
     public ArrayList<String> mUps;
     public Date mCreateAt;
     public String mReplyId;
+
+    public ModelReplie(JSONObject json) throws JSONException {
+
+        DateFormat df = new SimpleDateFormat(JSON_DATE_FORMAT, Locale.CHINA);
+
+        mReplieId = json.getString(JSON_ID);
+        mAuthor = new ModelAuthor(json.getJSONObject(JSON_AUTHOR));
+        mContent = json.getString(JSON_CONTENT);
+        mReplyId = json.getString(JSON_REPLY_ID);
+        try {
+            mCreateAt = df.parse(json.getString(JSON_CREATE_AT));
+        } catch (ParseException ignored) {
+        }
+
+        mUps = new ArrayList<>();
+        JSONArray jArray = json.getJSONArray(JSON_UP);
+        if (jArray != null) {
+            for (int i = 0; i < jArray.length(); i++) {
+                mUps.add(jArray.get(i).toString());
+            }
+        }
+    }
 
     public String getReplieId() {
         return mReplieId;

aacf214c15f2e8195d593a6fd17902a5c0e98718
YourtionGuo <yourtion@gmail.com>
2016-09-02 15:35:53 +0800

    Revert "use RecyclerView"
    This reverts commit 2166ee33cecfd68bef615fae2228ed865177ddd6.

--- app/src/main/java/com/yourtion/cnode_android/CNodeListAdapter.java
+++ app/src/main/java/com/yourtion/cnode_android/CNodeListAdapter.java
@@ -1,10 +1,10 @@
 package com.yourtion.cnode_android;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.yourtion.cnode_android.Modules.Topic;
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 /**
  * Created by Yourtion on 9/2/16.
  */
-public class CNodeListAdapter extends RecyclerView.Adapter<CNodeListAdapter.ViewHolder> {
+public class CNodeListAdapter extends BaseAdapter {
     private Context mContext;
     private ArrayList<Topic> mTopics;
 
@@ -28,28 +28,48 @@ public class CNodeListAdapter extends RecyclerView.Adapter<CNodeListAdapter.View
     }
 
     @Override
-    public int getItemCount() {
+    public int getCount() {
         if (mTopics == null) return 0;
         return mTopics.size();
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View convertView = LayoutInflater.from(parent.getContext()).inflate(
-                R.layout.topic_list_item, parent, false);
-        return new ViewHolder(convertView);
+    public Object getItem(int position) {
+        return mTopics.get(position);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = LayoutInflater.from(mContext).inflate(
+                    R.layout.topic_list_item, null);
+            holder.tab = (TextView) convertView.findViewById(R.id.list_item_tab);
+            holder.title = (TextView) convertView.findViewById(R.id.list_item_title);
+            holder.count = (TextView) convertView.findViewById(R.id.list_item_count);
+            holder.author = (TextView) convertView.findViewById(R.id.list_item_author);
+            holder.time = (TextView) convertView.findViewById(R.id.list_item_time);
+
+            // 将holder绑定到convertView
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
         Topic topic = mTopics.get(position);
         String tab_string = topic.getTabString();
 
-        if (topic.mTop || topic.mGood) {
+        if(topic.mTop || topic.mGood) {
             holder.tab.setBackgroundColor(mContext.getResources().getColor(R.color.colorTagH));
             holder.tab.setTextColor(mContext.getResources().getColor(R.color.colorTagTextH));
         }
-        if (tab_string != null) {
+        if(tab_string != null) {
             holder.tab.setText(topic.getTabString());
         } else {
             holder.tab.setVisibility(View.GONE);
@@ -59,23 +79,15 @@ public class CNodeListAdapter extends RecyclerView.Adapter<CNodeListAdapter.View
         holder.count.setText(topic.getCountText());
         holder.author.setText(topic.getAuthor().getLoginname());
         holder.time.setText(Utils.getTimeFormatText(topic.getLastReplyAt()));
+
+        return convertView;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    final class ViewHolder {
         TextView tab;
         TextView title;
         TextView count;
         TextView author;
         TextView time;
-
-        public ViewHolder(View convertView) {
-            super(convertView);
-            tab = (TextView) convertView.findViewById(R.id.list_item_tab);
-            title = (TextView) convertView.findViewById(R.id.list_item_title);
-            count = (TextView) convertView.findViewById(R.id.list_item_count);
-            author = (TextView) convertView.findViewById(R.id.list_item_author);
-            time = (TextView) convertView.findViewById(R.id.list_item_time);
-        }
     }
 }
-
--- app/src/main/java/com/yourtion/cnode_android/MainActivity.java
+++ app/src/main/java/com/yourtion/cnode_android/MainActivity.java
@@ -6,11 +6,10 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.yourtion.cnode_android.Modules.Topic;
 
@@ -19,8 +18,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private ListView mListView;
     private ArrayList<Topic> mTopics;
     private CNodeListAdapter mListAdapter;
 
@@ -40,13 +38,11 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.TopicList);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        mListView = (ListView) findViewById(R.id.TopicList);
+
 
         mListAdapter = new CNodeListAdapter(this, mTopics);
-        mRecyclerView.setAdapter(mListAdapter);
+        mListView.setAdapter(mListAdapter);
 
         CNodeClient client = new CNodeClient();
         client.getTopics("", 0, 0, new CNodeClient.Callback() {
@@ -60,9 +56,12 @@ public class MainActivity extends AppCompatActivity
                         mListAdapter.notifyDataSetChanged();
                     }
                 });
+
             }
+
             @Override
             public void fail() {
+
             }
         });
 

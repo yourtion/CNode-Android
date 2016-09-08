c4e71275379e4a2380247d50939871cf7062f790
YourtionGuo <yourtion@gmail.com>
2016-09-08 11:40:49 +0800

    back button

--- app/src/main/java/com/yourtion/cnode_android/ContentActivity.java
+++ app/src/main/java/com/yourtion/cnode_android/ContentActivity.java
@@ -2,6 +2,7 @@ package com.yourtion.cnode_android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
  * Created by Yourtion on 9/2/16.
@@ -11,5 +12,19 @@ public class ContentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

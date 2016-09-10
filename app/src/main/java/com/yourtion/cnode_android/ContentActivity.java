6d499e0daf0d7bafeb24a4015370edb168cb3637
YourtionGuo <yourtion@gmail.com>
2016-09-10 10:20:08 +0800

    add codeview

--- app/src/main/java/com/yourtion/cnode_android/ContentActivity.java
+++ app/src/main/java/com/yourtion/cnode_android/ContentActivity.java
@@ -2,11 +2,12 @@ package com.yourtion.cnode_android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.yourtion.cnode_android.Modules.Topic;
+
+import thereisnospon.codeview.CodeView;
 
 /**
  * Created by Yourtion on 9/2/16.
@@ -14,20 +15,20 @@ import com.yourtion.cnode_android.Modules.Topic;
 public class ContentActivity extends AppCompatActivity {
 
     private TextView mTitleTextView;
-    private TextView mContentTextView;
+    private CodeView mContentTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
-        if(getSupportActionBar() != null) {
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         mTitleTextView = (TextView) findViewById(R.id.text_view_title);
-        mContentTextView = (TextView) findViewById(R.id.text_view_content);
+        mContentTextView = (CodeView) findViewById(R.id.text_view_content);
         Topic topic = (Topic) getIntent().getSerializableExtra(MainActivity.TOPIC_KEY);
         mTitleTextView.setText(topic.getTitle());
-        mContentTextView.setText(Html.fromHtml(topic.getContent()));
+        mContentTextView.showCodeHtmlByCssSelect(topic.getContent(), "code");
     }
 
     @Override
